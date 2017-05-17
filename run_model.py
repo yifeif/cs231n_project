3dac9c7f06a67e491459a62a0259b6b136696e07
Hanna Revinskaya <annarev@google.com>
2017-05-16 17:32:49 -0700

    Make data split dir argument not required and call tf.global_variables_initializer

--- run_model.py
+++ run_model.py
@@ -7,13 +7,18 @@ import tensorflow as tf
 
 
 def main():
+  data_split_dir = FLAGS.data_split_dir
+  if not data_split_dir:
+    data_split_dir = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), 'data')
   num_epoch = 1 # 10
 
-  train_models_file = os.path.join(FLAGS.data_split_dir, 'train_data.txt')
+  train_models_file = os.path.join(data_split_dir, 'train_data.txt')
   edges_batch, images_batch = (
       data_loader.input(FLAGS.screenshots_dir, train_models_file, batch_size=16))
 
   with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
     tf.train.start_queue_runners(sess=sess)
     model.run_a_gan(sess, edges_batch, images_batch, num_epoch=num_epoch)
 
@@ -26,7 +31,7 @@ if __name__ == '__main__':
       '--screenshots_dir', type=str, default=None, required=True,
       help='Path to the screenshots directory containing data images.')
   parser.add_argument(
-      '--data_split_dir', type=str, default=None, required=True,
+      '--data_split_dir', type=str, default=None, required=False,
       help='Path to directory that contains test_data.txt, '
            'val_data.txt and train_data.txt files.')
 
